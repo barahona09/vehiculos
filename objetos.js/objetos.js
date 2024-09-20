d6fe91136271228ad6228ff326b3cc7a108e2309
barahona09 <cbarahona938@gmail.com>
2024-09-20 13:57:47 -0600

    vehiculos

---
+++ objetos.js/objetos.js
@@ -0,0 +1,39 @@
+/*objetos persona*/
+const persona = {
+    /*atributos persona*/
+    nombre: "carlos",
+    altura: 165,
+    color_piel: "morena",
+
+    /*metodos */
+
+};
+
+
+console.log("hola, soy" + persona.nombre);
+console.log("altura" + persona.altura);
+console.log("color_piel" + persona.color_piel);
+
+
+
+
+/*atributos*/
+console.log(carro.fabricante);
+console.log(carro.modelo);
+console.log(carro.año);
+
+let taza = {
+    color: "blanco",
+    forma: "redondo",
+    tamaño: 10,
+};
+
+console.log(taza.color);
+console.log(taza.forma);
+console.log(taza.tamaño);
+let carro = {
+    fabricante: "Honda",
+    modelo: "NSX 2016",
+    año: 2016,
+
+};
